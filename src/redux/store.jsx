c510db7d63bc925aa1e0b72455b2b7ced821c9be
gitoff-start <replyban@gmail.com>
2022-04-01 12:42:26 +0500

    add

---
+++ src/redux/store.jsx
@@ -0,0 +1,10 @@
+import rootReducers from "./reducer";
+import { createStore,compose, applyMiddleware } from "redux";
+//import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from "redux-logger";
+
+// for redux tool debugging 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducers,composeEnhancers(applyMiddleware(logger)));
+
+export default store;
